820ea1faf0be40c814734ef1efa7a05a1df9a43d
HannuMoller <moller.hannu@gmail.com>
2017-03-06 15:22:48 +0200

    IIS Tutorial exercise

---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Results;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+       {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M },
+            new Product { Id = 4, Name = "Mushroom Soup", Category = "Groceries", Price = 2.50M },
+            new Product { Id = 5, Name = "Picachu", Category = "Toys", Price = 333.85M },
+            new Product { Id = 6, Name = "Scissors", Category = "Hardware", Price = 12.95M }
+       };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public JsonResult<Product[]> GetAllProducts()
+        {
+            return Json(products);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Json(product);
+        }
+    }
+}
