aed9bb6b389c4616fec9578b60bb6e320c85508f
HannuMoller <moller.hannu@gmail.com>
2017-03-06 14:24:15 +0200

    ASP.NET tutorial exercise

---
+++ MvcMovie/src/MvcMovie/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Release Date"),
+         DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; }
+
+        // [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = false)]
+        [Range(1, 100), DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"), StringLength(5)]
+        public string Rating { get; set; }
+    }
+
+}
