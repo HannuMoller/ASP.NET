98debdfbc1d080607f1b425d7c2edeccd985acd6
HannuMoller <moller.hannu@gmail.com>
2017-03-07 09:17:38 +0200

    unnecessary using clauses removed

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Http;
-using System.Web.Http.Results;
 using ProductsApp.Models;
 
 namespace ProductsApp.Controllers
